2f4fd36528cc931757b36a8dd0949b066da405e7
Ryan William Tierney <ryan.tierney92@gmail.com>
2019-10-13 14:55:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,12 @@
 def oxford_comma(array)
   if array.size > 2 
-<<<<<<< HEAD
     array[-1].insert(0, "and ")
     array.join(", ")
   else 
     array.join(" and ")    
-=======
     array.join(", ")
     
   else 
     array[-1].insert(0, "and ")
->>>>>>> af96dae91fd45b0e9a2d19b90412a3297c75ceba
 end
 end
